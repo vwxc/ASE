cc218c3c581a2b5c1b5447edf774d2aeb25291a9
Bilsashikel <beelee93@gmail.com>
2016-08-22 15:19:40 +0800

    Created LibGDX java project

---
+++ src/desktop/src/com/subspace/cerebria/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.subspace.cerebria.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.subspace.cerebria.CerebriaGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CerebriaGame(), config);
+	}
+}
